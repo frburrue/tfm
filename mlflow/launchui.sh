b781deb822f3ec825d66f8ebfd49610e24be8f2f
root <root@ip-172-31-33-104.eu-west-3.compute.internal>
2020-08-31 12:07:44 +0000

    Add manifests

--- mlflow/launchui.sh
+++ mlflow/launchui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mlflow server -h 0.0.0.0 -p 8990 --backend-store-uri $MLFLOW_TRACKING_URI --default-artifact-root $(pwd)/artifacts
+mlflow server -h 0.0.0.0 -p 61001 --backend-store-uri $MLFLOW_TRACKING_URI --default-artifact-root s3://mlflow-tfm
--- mlflow/setenv.sh
+++ mlflow/setenv.sh
@@ -1,1 +1,1 @@
-export MLFLOW_TRACKING_URI='postgresql://francisco:francisco@localhost:60220/mlflow'
+export MLFLOW_TRACKING_URI='postgresql://francisco:francisco@mlflow.c3uusfqnmup4.eu-west-3.rds.amazonaws.com/mlflow'
