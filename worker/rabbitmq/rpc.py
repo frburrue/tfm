8b05c14c292ad14ed479be7110139c53c656f8f9
frburrue <frburrue@ucm.es>
2020-08-31 16:19:27 +0200

    Update rpc.py

--- worker/rabbitmq/rpc.py
+++ worker/rabbitmq/rpc.py
@@ -39,54 +39,57 @@ class RpcWorker:
         self.channel.start_consuming()
 
     def on_request(self, ch, method, props, body):
+        
+        try:
+            data = pickle.loads(body)
+            model_name = data['model']
+            response = {}
+            model = None # Model
+            print("Let's go!!!")
+            for mv in MLFLOW_CLIENT.search_model_versions(f"name='{model_name}'"):
+                mv = dict(mv)
+                if mv['current_stage'] == 'Production':
+                    mv['last_updated_timestamp'] = str(datetime.fromtimestamp(int(mv['last_updated_timestamp'] / 1000)))
+                    if os.path.exists(mv['last_updated_timestamp']):
+                        print("Load existing model...")
+                        model = mlflow.keras.load_model(mv['last_updated_timestamp'])
+                    else:
+                        print("Downloading model...")
+                        model = mlflow.keras.load_model(mv['source'])
+                        mlflow.keras.save_model(model, mv['last_updated_timestamp'])
+                    print("Using model {name} v{version} ({current_stage}) updated at {last_updated_timestamp}".format(**mv))
+                    response = {k: v for k, v in mv.items() if v}
+                    break
 
-        data = pickle.loads(body)
-        model_name = data['model']
-        response = {}
-        model = None # Model
+            if model:
+                idx = str(uuid.uuid4())
+                open(str(idx), 'wb').write(data['data'])
+                img = image.load_img(idx, target_size=(img_width, img_height))
+                os.unlink(idx)
+                x = image.img_to_array(img)
+                x = np.expand_dims(x, axis=0)
+                response['prediction'] = int(np.argmax(model.predict_proba(x)))
+                response['text'] = MONGO_CLIENT.get_one(model_name, response['prediction'])
 
-        for mv in MLFLOW_CLIENT.search_model_versions(f"name='{model_name}'"):
-            mv = dict(mv)
-            if mv['current_stage'] == 'Production':
-                mv['last_updated_timestamp'] = str(datetime.fromtimestamp(int(mv['last_updated_timestamp'] / 1000)))
-                if os.path.exists(mv['last_updated_timestamp']):
-                    print("Load existing model...")
-                    model = mlflow.keras.load_model(mv['last_updated_timestamp'])
-                else:
-                    print("Downloading model...")
-                    model = mlflow.keras.load_model(mv['source'])
-                    mlflow.keras.save_model(model, mv['last_updated_timestamp'])
-                print("Using model {name} v{version} ({current_stage}) updated at {last_updated_timestamp}".format(**mv))
-                response = {k: v for k, v in mv.items() if v}
-                break
+            else:
+                response = {'error': 'service unavailable'}
 
-        if model:
-            idx = str(uuid.uuid4())
-            open(str(idx), 'wb').write(data['data'])
-            img = image.load_img(idx, target_size=(img_width, img_height))
-            os.unlink(idx)
-            x = image.img_to_array(img)
-            x = np.expand_dims(x, axis=0)
-            response['prediction'] = int(np.argmax(model.predict_proba(x)))
-            response['text'] = MONGO_CLIENT.get_one(model_name, response['prediction'])
+            response = {
+                'payload': response,
+                'worker_id': self.id
+            }
 
-        else:
-            response = {'error': 'service unavailable'}
-
-        response = {
-            'payload': response,
-            'worker_id': self.id
-        }
-
-        self.channel.basic_publish(
-            exchange='',
-            routing_key=props.reply_to,
-            properties=pika.BasicProperties(
-                correlation_id=props.correlation_id
-            ),
-            body=json.dumps(response)
-        )
-        ch.basic_ack(delivery_tag=method.delivery_tag)
+            self.channel.basic_publish(
+                exchange='',
+                routing_key=props.reply_to,
+                properties=pika.BasicProperties(
+                    correlation_id=props.correlation_id
+                ),
+                body=json.dumps(response)
+            )
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+        except Exception as e:
+            print(str(e))
 
     def release(self):
         self.connection.close()
