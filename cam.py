987fef802ec15ad2a8de6a62ddfa93332b5301be
Francisco Burruezo Aranda <francisco.burruezo@accenture.com>
2020-10-04 15:00:41 +0200

    Changes

--- cam.py
+++ cam.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import requests
 import base64
 from PIL import Image, ImageDraw
@@ -6,7 +7,7 @@ import tempfile
 
 def inference(image_path):
     try:
-        url = "http://ec2-15-188-9-114.eu-west-3.compute.amazonaws.com/rpc/Hands"
+        url = "http://ec2-15-188-9-114.eu-west-3.compute.amazonaws.com:60211/rpc/Hands"
 
         payload = {}
         files = [
@@ -16,7 +17,7 @@ def inference(image_path):
 
         response = requests.request("POST", url, headers=headers, data=payload, files=files).json()
 
-        print("Elapsed time: %d seconds" % response['elapsed'])
+        print("Elapsed time: %f seconds" % response['elapsed'])
         img = response['response']['payload']['result'][0]['image']
         predictions = response['response']['payload']['result'][0]['predictions']
 
@@ -30,9 +31,13 @@ def inference(image_path):
                 shape = list(map(lambda x: tuple(x), p[:2]))
                 img1 = ImageDraw.Draw(img)
                 img1.rectangle(shape, outline="red")
-        img.show()
+
         fp.close()
+
         print("DONE!")
+
+        return np.array(img)
+
     except Exception as e:
         print(str(e))
 
@@ -40,22 +45,29 @@ def cam():
 
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
+    vc.set(cv2.CAP_PROP_BUFFERSIZE, 0)
 
     if vc.isOpened(): # try to get the first frame
         rval, frame = vc.read()
+        new_frame = frame
     else:
         rval = False
 
     while rval:
+
         cv2.imshow("preview", frame)
+        cv2.imshow("detection", new_frame)
         rval, frame = vc.read()
+
         key = cv2.waitKey(20)
         if key == 27: # exit on ESC
             break
         elif key == 115:
             print("OK!")
             cv2.imwrite('this.png', frame)
-            inference('this.png')
+            new_frame = inference('this.png')
+
     cv2.destroyWindow("preview")
+    cv2.destroyWindow("detection")
 
 cam()
