209e9719d9c43432f69d2c6987a45138b0cb9046
Francisco Burruezo Aranda <francisco.burruezo@accenture.com>
2020-12-06 14:47:55 +0100

    gunicorn ln

--- mlflow/mlflow-environment/docker_entrypoint.sh
+++ mlflow/mlflow-environment/docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/miniconda3/envs/mlflow/bin/mlflow server -h 0.0.0.0 -p 8990 --backend-store-uri $MLFLOW_TRACKING_URI --default-artifact-root $MLFLOW_ARTIFACTS_PATH
+mlflow server -h 0.0.0.0 -p 8990 --backend-store-uri $MLFLOW_TRACKING_URI --default-artifact-root $MLFLOW_ARTIFACTS_PATH
