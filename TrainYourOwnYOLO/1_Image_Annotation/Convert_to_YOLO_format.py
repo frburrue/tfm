c8ea0fd477629cd34fef9deede1b698e5e5cb30a
Francisco Burruezo Aranda <frburrue@ucm.es>
2021-01-03 00:43:35 +0000

    Update

--- TrainYourOwnYOLO/1_Image_Annotation/Convert_to_YOLO_format.py
+++ TrainYourOwnYOLO/1_Image_Annotation/Convert_to_YOLO_format.py
@@ -21,7 +21,7 @@ from Convert_Format import convert_vott_csv_to_yolo
 
 Data_Folder = os.path.join(get_parent_dir(1), "Data")
 VoTT_Folder = os.path.join(
-    Data_Folder, "Source_Images", "Training_Images", "vott-csv-export-augmented-check"
+    Data_Folder, "Source_Images", "Training_Images", "vott-csv-export-new-parsed"
 )
 VoTT_csv = os.path.join(VoTT_Folder, "Annotations-export.csv")
 YOLO_filename = os.path.join(VoTT_Folder, "data_train.txt")
